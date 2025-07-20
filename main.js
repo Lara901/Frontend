e19f65251c3fbf63d76ca8c7079593b693a2c873
Lara901 <larasebas10.2005@gmail.com>
2025-07-20 14:31:16 -0500

    Primer commit del frontend

---
+++ main.js
@@ -0,0 +1,23 @@
+const Form = document.getElementById('loginForm');
+Form.addEventListener('submit', function (e) {
+    e.preventDefault();
+const usuario = document.getElementById('usuario').value;
+    const clave = document.getElementById('clave').value;
+  fetch('https://backend-login-mfsm.onrender.com', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+
+    body: JSON.stringify({ usuario, clave })
+  }) .then(res => res.json())
+      .then(data => {
+        if (data.acceso === true) {
+         localStorage.setItem('usuarioAutenticado', 'true');
+  window.location.href = 'index.html';
+        }else {
+            document.getElementById('mensaje').innerText = '❌ Acceso denegado';
+        }
+      }) .catch (err => {
+    document.getElementById('mensaje').innerText = '⚠️ Error en el servidor';
+        console.error(err);
+});
+}); 
